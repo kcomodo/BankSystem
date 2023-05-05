2967bd0718e949f2ead95c5e6ad2f63899aed490
Quang <mxrshmalloww@gmail.com>
2023-05-05 15:31:16 -0400

    Added server.js

---
+++ Bank/server.js
@@ -0,0 +1,34 @@
+'use strict';
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+const mysql = require('mysql2');
+
+const con = mysql.createConnection({
+    host: "istwebclass.org",
+    user: "anewbaue_CatExposure",
+    password: "Megumin56",
+    database: "anewbaue_Bank"
+
+});
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+
+
+app.listen(app.get('port'), function () {
+    console.log('Server started: http://localhost/:' + app.get('port') + '/frontEnd/homepage.html');
+});
