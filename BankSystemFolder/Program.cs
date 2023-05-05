7f4f808c288a8f2bb5a7219df1e7a38def4b7acc
Quang <mxrshmalloww@gmail.com>
2023-05-05 12:27:53 -0400

    Added some comments

--- BankSystemFolder/Program.cs
+++ BankSystemFolder/Program.cs
@@ -9,16 +9,27 @@ class Program
         InfoRepository program = new InfoRepository();
         BankServices bank = new BankServices();
         
-        
+        /*
+        NOTE: Branch out when creating a method for the user story
+        User stories are in the bankservices.cs
+        TODO:
+        Create login for username and password, and other info.
+        Create username and password validation
+        Save info in database
+        Assign ID to account created
+        Check login to see if it's valid
+        -----------------------------------------------------------
+        LATER ON: 
+        Unit test user story that have been created.
+        .NET Webservice to modify and add data, rather than manually changing from sql
+        Banking website
+
+        */
         IEnumerable<InfoModel> list = bank.ReturnInfoName();
         foreach(InfoModel info in list){
                 Console.WriteLine(info.ToString());
         }
 
-        /*
-        IEnumerable<InfoModel> list = program.ReturnNamesFake();
-        Console.WriteLine(list);
-        */
         }
        
         
--- BankSystemFolder/Services/BankServices.cs
+++ BankSystemFolder/Services/BankServices.cs
@@ -8,6 +8,13 @@ namespace MyProjects.Services{
         public BankServices(){
             _repo = new InfoRepository();
         }
+
+        /*User stories:
+        1. As a customer, I would like to check my deposits
+        2. As a customer, I would like to view my charges
+        3. As a customer, I would like to view all of my withdrawals
+          
+        */
         public List<InfoModel> ReturnInfoName(){
             List<InfoModel> infoNames = _repo.ReturnName().ToList<InfoModel>();
             return infoNames;
