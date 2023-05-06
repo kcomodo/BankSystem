4f86776d60a5775e535687faf754554ddd8ead58
CatExposure <andrenewbauer@gmail.com>
2023-05-06 17:33:24 -0400

    Made a public folder because I think it needs that?

--- Bank_Web/server.js
+++ Bank_Web/server.js
@@ -30,5 +30,5 @@ con.connect(function(err) {
 
 
 app.listen(app.get('port'), function () {
-    console.log('Server started: http://localhost/:' + app.get('port') + '/frontEnd/homepage.html');
+    console.log('Server started: http://localhost:' + app.get('port') + '/Front-End/Home.html');
 });
