5c68e92a4c41e325c8f1b351782f4c339a8948b0
CatExposure <andrenewbauer@gmail.com>
2023-05-10 18:00:29 -0400

    Worked on css formatting and navBar, cut short due to database security being high priority

--- bank_website/src/components/Layout.js
+++ bank_website/src/components/Layout.js
@@ -3,32 +3,8 @@ import '../styles/Layout.css';
 import React, {useState} from 'react';
 
 function Layout() {
-  const [open, setOpen] = useState(false);
-
   return (
-    <>
-      <nav className= "navBar">
-            <Link to="/" className = {"link-styles"}>Home</Link>
-                <div className="dropdown-profile" onClick={()=>{setOpen(!open);}}>Profile</div>
-              <div className={`dropdown-menu ${open? 'active' : 'inactive'}`}>
-                <ul>
-                  <DropdownItem link = {"/blogs"} linkName = {"Blogs"}/>
-                  <DropdownItem link = {"/Password"} linkName = {"Password"}/>
-                </ul>
-              </div>
-            <Link to="/contact" className = {"link-styles"}>Contact</Link>
-      </nav>
-      <Outlet />
-    </>
-  )
-};
-
-function DropdownItem(props) {
-  return (
-    <li className = "dropdownItem">
-      <Link to={props.link} className = {"link-styles"}>{props.linkName}</Link>
-    </li>
+    <h1>a</h1>
   );
-}
-
+  }
 export default Layout;
