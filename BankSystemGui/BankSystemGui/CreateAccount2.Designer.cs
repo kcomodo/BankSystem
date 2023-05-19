8cae680ddfd0996f935e25fe8fba77a9fd0b221f
Quang <mxrshmalloww@gmail.com>
2023-05-18 20:23:55 -0400

    finished up createaccount2 ui

--- BankSystemGui/BankSystemGui/CreateAccount2.Designer.cs
+++ BankSystemGui/BankSystemGui/CreateAccount2.Designer.cs
@@ -29,42 +29,12 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CreateAccount2));
-            this.label1 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.txtCityCreate = new System.Windows.Forms.TextBox();
             this.txtZipCreate = new System.Windows.Forms.TextBox();
             this.txtAddressCreate = new System.Windows.Forms.TextBox();
             this.btnCreateAccount = new System.Windows.Forms.Button();
             this.stateCombo = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(45, 151);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(30, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "City: ";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(45, 180);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(25, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Zip:";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(45, 203);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(48, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Address:";
             // 
             // txtCityCreate
             // 
@@ -115,15 +85,12 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(520, 450);
+            this.ClientSize = new System.Drawing.Size(441, 450);
             this.Controls.Add(this.stateCombo);
             this.Controls.Add(this.btnCreateAccount);
             this.Controls.Add(this.txtAddressCreate);
             this.Controls.Add(this.txtZipCreate);
             this.Controls.Add(this.txtCityCreate);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "CreateAccount2";
             this.Text = "Form1";
@@ -133,10 +100,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtCityCreate;
         private System.Windows.Forms.TextBox txtZipCreate;
         private System.Windows.Forms.TextBox txtAddressCreate;
